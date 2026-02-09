2d55716723a9dd5900136662bce464ff0cfd81c5
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 16:01:37 +0530

    Add files via upload

---
+++ difference of two integers using-.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a=8;
+    int b=3;
+    int sub=a-b;
+    printf("%d",sub);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int a=10;
+    int b=5;
+    int sub=a-b;
+    printf("%d",sub);
+    return 0;
+}
