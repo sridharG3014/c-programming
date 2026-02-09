78737f460e3b5e51859b5bf3d149bb221f424c59
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 14:20:04 +0530

    Add files via upload

---
+++ two integer in one line.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    int b=10;
+    printf("%d %d",a,b);
+    return 0;
+}
+
+#include<stdio.h>
+int main(){
+    int a=20;
+    int b=30;
+    printf("%d %d",a,b);
+    return 0;
+}
---
+++ two variable in one line.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+ int a=5;
+ int b=15;
+ printf("%d %d",a,b);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+ int a=20;
+ int b=40;
+ printf("%d %d",a,b);
+    return 0;
+}
