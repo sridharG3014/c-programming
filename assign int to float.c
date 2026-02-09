5b4d020618ef3f5fe35077950bb15a39364757ed
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 15:43:42 +0530

    Add files via upload

---
+++ assign int to float.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a=10;
+    float b;
+    b=(float)a;
+    printf("%.1f",b);
+    return 0;
+
+
+
+#include<stdio.h>
+int main(){
+    int a=25;
+    float b;
+    b=(float)a;
+    printf("%.1f",b);
+    return 0;
+}
