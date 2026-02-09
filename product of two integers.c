1aef5454c20567db266842c11e3665de274ff3c2
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 16:05:27 +0530

    Add files via upload

---
+++ product of two integers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int a=4;
+    int b=5;
+    int product=a*b;
+    printf("%d",product);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int a=10;
+    int b=2;
+    int product=a*b;
+    printf("%d",product);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    int a=1;
+    int b=20;
+    int product=a*b;
+    printf("%d",product);
+    return 0;
+}
