24d623c8e615e3a77a611cf7f281e067cdb95c9c
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 16:36:54 +0530

    Add files via upload

---
+++ quotient and remainder.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    int a,b,quotient,remainder1;
+    scanf("%d %d",&a,&b);
+    quotient=a/b;
+    remainder1=a%b;
+    printf("quotient=");
+    printf("%d ",quotient);
+    printf("remainder=");
+    printf("%d",remainder1);
+    return 0;
+}
+
+input=9,3
+
+
+#include<stdio.h>
+int main(){
+    int a,b,quotient,remainder1;
+    scanf("%d %d",&a,&b);
+    quotient=a/b;
+    remainder1=a%b;
+    printf("quotient=");
+    printf("%d ",quotient);
+    printf("remainder=");
+    printf("%d",remainder1);
+    return 0;
+}
+
+input=10 3
+
+
+
+#include<stdio.h>
+int main(){
+    int a,b,quotient,remainder1;
+    scanf("%d %d",&a,&b);
+    quotient=a/b;
+    remainder1=a%b;
+    printf("quotient=");
+    printf("%d ",quotient);
+    printf("remainder=");
+    printf("%d",remainder1);
+    return 0;
+}
+
+
+input=20 4
