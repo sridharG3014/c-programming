2b27e9a4f68414ec4a55791d1c7243b3a3fb9ee9
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 15:30:17 +0530

    Add files via upload

---
+++ size of the float.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    float a;
+    printf("%zu",sizeof(float));
+    return 0;
+}
+
+
+#include<stdio.h>
+int main(){
+    float z;
+    printf("%zu",sizeof(float));
+    return 0;
+}
