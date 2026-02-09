b1f02944e85086ccc9f013345d78bcd9cfeabd55
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-09 15:57:57 +0530

    Add files via upload

---
+++ print two sum integers using.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    int b=5;
+    int sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
+
+
+
+#include<stdio.h>
+int main(){
+    int a=3;
+    int b=7;
+    int sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
