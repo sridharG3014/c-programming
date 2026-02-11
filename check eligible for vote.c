0088f80c7018f51bc98201379a4a40dfb9c76a36
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-11 16:01:33 +0530

    Add files via upload

---
+++ check eligible for vote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num>=18 && num<=18)
+    {
+        printf("Eligible for Vote");
+    }
+    else{
+        printf("Not Eligible for Vote");
+    }
+        return 0;
+}
