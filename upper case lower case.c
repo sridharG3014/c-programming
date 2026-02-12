008956c7132068aa391161bc30ad62d6669a24ff
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-12 11:46:33 +0530

    Add files via upload

---
+++ upper case lower case.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch>=65 && ch<=90){
+        printf("Upper Case");
+    }
+    else if(ch>=97 && ch<=122){
+        printf("Lower Case");
+    }
+    else{
+        printf("Invalid");
+    }
+    return 0;
+}
