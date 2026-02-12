5be4f4c14de1411472db0ad0f97d886160317e85
sridharG3014 <sridharkeerthika007@gmail.com>
2026-02-12 12:22:51 +0530

    Add files via upload

---
+++ equivalent triangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a==b&&b==c){
+        printf("Equialent Triangle");
+    }
+    else{
+        printf("not Equivalent Triangle");
+    }
+    
+    return 0;
+}
