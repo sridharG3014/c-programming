48b142721ca8e74ea6518e5cdb9683dffcd9cbdc
sridharG3014 <sridharkeerthika007@gmail.com>
2026-03-02 09:19:19 +0530

    Add files via upload

---
+++ Pattern printing Q6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){ 
+       
+        for(int j=1;j<=i;j++){
+        
+            if(i%2!=0){
+                if(j%2==0)
+           printf("0");
+       
+        else{
+            printf("1");
+        }
+            }
+       else{
+           if(j%2==0){
+           printf("1");
+           }
+           else{
+               printf("0");
+           }
+       }
+       
+        }
+        printf("\n");
+    
+    }
+    return 0;
+}
